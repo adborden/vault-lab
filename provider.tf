269116abe3bdd474c8b5a532dd965a22ee82f543
Aaron D Borden <adborden@a14n.net>
2026-05-07 22:55:16 -0700

    deps: add version constraints

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = "~> 1.3"
+
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 5.7.0"
+    }
+  }
+}
+
 provider "vault" {
   address = "https://vault.fl.a14n.net"
 }
